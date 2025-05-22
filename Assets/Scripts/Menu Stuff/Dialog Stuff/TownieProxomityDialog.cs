18dc94e614d8d48e05c647c592d25c3a33b3901c
Chris Kulwik <ckulwik@umich.edu>
2025-05-22 10:39:35 -0400

    render different dialog when talking to different people

--- Assets/Scripts/Menu Stuff/Dialog Stuff/TownieProxomityDialog.cs
+++ Assets/Scripts/Menu Stuff/Dialog Stuff/TownieProxomityDialog.cs
@@ -2,8 +2,36 @@ using UnityEngine;
 
 public class TownieProxomityDialog : MonoBehaviour
 {
+    public enum TownieId
+    {
+        Townie1,
+        Townie2,
+    }
+
+    [System.Serializable]
+    public class TownieDialog
+    {
+        public TownieId townieId;
+        public string[] dialogLines;
+    }
+
+    public TownieId thisTownieId;
     private bool canOpenDialog = false;
     public GameObject dialogBox;
+    private TownieDialog[] townieDialogs = new TownieDialog[]
+    {
+        new TownieDialog { townieId = TownieId.Townie1, dialogLines = new string[] { "Hello, traveler!", "I am townie number 1" } },
+        new TownieDialog { townieId = TownieId.Townie2, dialogLines = new string[] { "Hi there!", "I am townie number 2" } },
+    };
+
+    private DialogController dialogController;
+
+    private void Start()
+    {
+        dialogController = dialogBox.GetComponent<DialogController>();
+    }
+
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -24,14 +52,18 @@ public class TownieProxomityDialog : MonoBehaviour
 
     private void Update()
     {
-        if (canOpenDialog)
+        if (canOpenDialog && Input.GetMouseButtonDown(1))
         {
-            if (Input.GetMouseButtonDown(1))
+            // Find the dialog lines for this townie
+            foreach (var townieDialog in townieDialogs)
             {
-                dialogBox.SetActive(true);
+                if (townieDialog.townieId == thisTownieId)
+                {
+                    dialogController.lines = townieDialog.dialogLines;
+                    dialogBox.SetActive(true);
+                    break;
+                }
             }
         }
     }
-
-    
 }
